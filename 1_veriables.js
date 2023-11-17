3de6b4fc06a5981dcef496a926d5488994515637
Lliicissst <german.popko111@gmail.com>
2023-11-17 23:25:42 +0100

    ending_1

--- 1_veriables.js
+++ 1_veriables.js
@@ -1,7 +1,18 @@
 //! let vs const, naming snake_case, SCREAMING_SNAKE_CASE, camelCase
 
+// const NUMBER_ONE = true;
+// let numberTwo = 2;
+
+// numberTwo = "1";
 //? dynamic typing
 
 // visible scope, function 'hello' example
+function hello(name) {
+  let hi = "Hello ";
+  console.log(hi + name + "!");
+}
+let yourName = "Herman";
 
-console.log('Hello world!')
+hello(yourName);
+// console.log(hi);
+// console.log(NUMBER_ONE + numberTwo);
